64fa691dc03665c4a6c58ef8064e3429c9f6438e
HeroGian <brilli.gianluca@gmail.com>
2015-05-24 17:50:00 +0200

    modifiche allo stemming

--- main.py
+++ main.py
@@ -1,8 +1,25 @@
-import re,math,collections,itertools
-import nltk, nltk.classify.util,nltk.metrics
-from nltk.classify import NaiveBayesClassifier
-from nltk.metrics import BigramAssocMeasures
-from nltk.probability import FreqDist, ConditionalFreqDist
+import nltk, nltk.classify.util, nltk.metrics
+from nltk.corpus import stopwords
+
+# Ritorna una lista contenente tutte le parole
+# contenute nel training set
+def get_words_in_tweets(tweets):
+    all_words = []
+    for (words, sentiment) in tweets:
+      all_words.extend(words)
+    return all_words
+
+def get_word_features(wordlist):
+    wordlist = nltk.FreqDist(wordlist)
+    word_features = wordlist.keys()
+    return word_features
+
+def extract_features(document):
+    document_words = set(document)
+    features = {}
+    for word in word_features:
+        features['contains(%s)' % word] = (word in document_words)
+    return features
 
 pos_tweets = [('I love this car', 'positive'),
               ('It will be easy','positive'),
@@ -19,41 +36,28 @@ neg_tweets = [('I do not like this car', 'negative'),
               ('I am not looking forward to the concert', 'negative'),
               ('He is my enemy', 'negative')]
 
-tweets = []
-for (words, sentiment) in pos_tweets + neg_tweets:
-    words_filtered = [e.lower() for e in words.split() if len(e) >= 3]
-    tweets.append((words_filtered, sentiment))
+# Creazione di una lista contenente un array di parole prive di stopwords
+# con accanto la classificazione positivo/negativo
+wnl = nltk.WordNetLemmatizer()
 
-print tweets
+test_tweets = []
+for (phrase, sentiment) in pos_tweets + neg_tweets:
+    tokens = nltk.word_tokenize(phrase)
+    for t in tokens:
+        if t in stopwords.words('english'):
+            tokens.remove(t)
+    test_tweets.append((tokens, sentiment))
 
+print test_tweets
+print get_words_in_tweets(test_tweets)
+print get_word_features(get_words_in_tweets(test_tweets))
 
-def get_words_in_tweets(tweets):
-    all_words = []
-    for (words, sentiment) in tweets:
-      all_words.extend(words)
-    return all_words
+word_features = get_word_features(get_words_in_tweets(test_tweets))
 
-def get_word_features(wordlist):
-    wordlist = nltk.FreqDist(wordlist)
-    word_features = wordlist.keys()
-    return word_features
-
-word_features = get_word_features(get_words_in_tweets(tweets))
-
-print word_features
-
-
-def extract_features(document):
-    document_words = set(document)
-    features = {}
-    for word in word_features:
-        features['contains(%s)' % word] = (word in document_words)
-    return features
-
-training_set = nltk.classify.apply_features(extract_features, tweets)
+training_set = nltk.classify.apply_features(extract_features, test_tweets)
 classifier = nltk.NaiveBayesClassifier.train(training_set)
 
-tweet = 'this day is a bad day'
-tweet = tweet.lower()
+tweet = 'I do not really love this work'.lower()
+
 print tweet
 print classifier.classify(extract_features(tweet.split()))
