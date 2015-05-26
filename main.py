8fda60fefa94983d643ea15701271c00fedf9fc5
HeroGian <brilli.gianluca@gmail.com>
2015-05-26 18:21:55 +0200

    aggiunta la probabilità

--- main.py
+++ main.py
@@ -1,19 +1,31 @@
-import nltk, nltk.classify.util, nltk.metrics
+import nltk
+import nltk.classify.util
+import nltk.metrics
+
 from nltk.corpus import stopwords
 
-# Ritorna una lista contenente tutte le parole
-# contenute nel training set
+
+# Dato un insieme di tweets(training set) ritorna una lista
+# contenente solo le parole.
 def get_words_in_tweets(tweets):
     all_words = []
     for (words, sentiment) in tweets:
-      all_words.extend(words)
+        all_words.extend(words)
     return all_words
 
+
+# Data una lista di parole, elimina le parole ripetute
+# e ritorna una lista ordinata per frequenza di parola.
 def get_word_features(wordlist):
     wordlist = nltk.FreqDist(wordlist)
     word_features = wordlist.keys()
     return word_features
 
+
+# Crea un dizionario che indica per ogni parola contenuta nel training set
+# se e presente o meno nel tweet passato in input.
+# esempio:
+# {love: True, this: True, car: True, hate: False, Concert: False}
 def extract_features(document):
     document_words = set(document)
     features = {}
@@ -21,8 +33,9 @@ def extract_features(document):
         features['contains(%s)' % word] = (word in document_words)
     return features
 
+
 pos_tweets = [('I love this car', 'positive'),
-              ('It will be easy','positive'),
+              ('It will be easy', 'positive'),
               ('This view is amazing', 'positive'),
               ('I feel great this morning', 'positive'),
               ('I am so excited about the concert', 'positive'),
@@ -30,34 +43,36 @@ pos_tweets = [('I love this car', 'positive'),
 
 neg_tweets = [('I do not like this car', 'negative'),
               ('This view is horrible', 'negative'),
-              ('It will not be easy','negative'),
-              ('I hate this car','negative'),
+              ('It will not be easy', 'negative'),
+              ('I hate this car', 'negative'),
               ('I feel tired this morning', 'negative'),
               ('I am not looking forward to the concert', 'negative'),
               ('He is my enemy', 'negative')]
 
-# Creazione di una lista contenente un array di parole prive di stopwords
-# con accanto la classificazione positivo/negativo
-wnl = nltk.WordNetLemmatizer()
+if __name__ == "__main__":
+    # Creazione di una lista contenente un array di parole prive di stopwords
+    # con accanto la classificazione positivo/negativo
+    wnl = nltk.WordNetLemmatizer()
 
-test_tweets = []
-for (phrase, sentiment) in pos_tweets + neg_tweets:
-    tokens = nltk.word_tokenize(phrase)
-    for t in tokens:
-        if t in stopwords.words('english'):
-            tokens.remove(t)
-    test_tweets.append((tokens, sentiment))
+    test_tweets = []
+    for (phrase, sentiment) in pos_tweets + neg_tweets:
+        tokens = nltk.word_tokenize(phrase)
+        for t in tokens:
+            if t in stopwords.words('english'):
+                tokens.remove(t)
+        test_tweets.append((tokens, sentiment))
 
-print test_tweets
-print get_words_in_tweets(test_tweets)
-print get_word_features(get_words_in_tweets(test_tweets))
+    # Lista contenente le parole presenti nel training set ordinate per frequenza
+    word_features = get_word_features(get_words_in_tweets(test_tweets))
 
-word_features = get_word_features(get_words_in_tweets(test_tweets))
+    training_set = nltk.classify.apply_features(extract_features, test_tweets)
+    classifier = nltk.NaiveBayesClassifier.train(training_set)
 
-training_set = nltk.classify.apply_features(extract_features, test_tweets)
-classifier = nltk.NaiveBayesClassifier.train(training_set)
+    tweet = 'The beer is good'.lower()
 
-tweet = 'I do not really love this work'.lower()
+    prob_t = classifier.prob_classify(extract_features(tweet.split()))
 
-print tweet
-print classifier.classify(extract_features(tweet.split()))
+    print tweet
+    print "Classificazione:", prob_t.max()
+    print "Tweet positivo al", round(prob_t.prob('positive'), 2)*100, "%"
+    print "Tweet negativo al", round(prob_t.prob('negative'), 2)*100, "%"
